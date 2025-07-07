dc16b7b22806a28f07825823b0d01b8a09040c70
PWA-developer1 <enzematarciano@gmail.com>
2025-07-07 09:48:27 +0100

    Update and rename db.js to database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
-class MyPubDB {
+class Database {
     constructor() {
-        this.dbName = 'myPubDB';
+        this.dbName = 'mYpuBDB';
         this.dbVersion = 1;
         this.db = null;
         this.init();
@@ -21,31 +21,25 @@ class MyPubDB {
 
                 // Users store
                 if (!db.objectStoreNames.contains('users')) {
-                    const usersStore = db.createObjectStore('users', { keyPath: 'email' });
-                    usersStore.createIndex('by_name', 'fullName');
+                    const userStore = db.createObjectStore('users', { keyPath: 'email' });
+                    userStore.createIndex('fullname', 'fullname', { unique: false });
                 }
 
                 // Media store
                 if (!db.objectStoreNames.contains('media')) {
                     const mediaStore = db.createObjectStore('media', { keyPath: 'id', autoIncrement: true });
-                    mediaStore.createIndex('by_user', 'userId');
-                    mediaStore.createIndex('by_date', 'uploadDate');
-                }
-
-                // Likes store
-                if (!db.objectStoreNames.contains('likes')) {
-                    const likesStore = db.createObjectStore('likes', { keyPath: ['mediaId', 'userId'] });
-                    likesStore.createIndex('by_media', 'mediaId');
+                    mediaStore.createIndex('userId', 'userId', { unique: false });
+                    mediaStore.createIndex('timestamp', 'timestamp', { unique: false });
                 }
             };
         });
     }
 
-    async addUser(userData) {
+    async addUser(user) {
         return new Promise((resolve, reject) => {
             const transaction = this.db.transaction(['users'], 'readwrite');
             const store = transaction.objectStore('users');
-            const request = store.add(userData);
+            const request = store.add(user);
 
             request.onsuccess = () => resolve(request.result);
             request.onerror = () => reject(request.error);
@@ -67,18 +61,12 @@ class MyPubDB {
         return new Promise((resolve, reject) => {
             const transaction = this.db.transaction(['media'], 'readwrite');
             const store = transaction.objectStore('media');
-            const request = store.add(mediaData);
-
-            request.onsuccess = () => resolve(request.result);
-            request.onerror = () => reject(request.error);
-        });
-    }
-
-    async getMedia(id) {
-        return new Promise((resolve, reject) => {
-            const transaction = this.db.transaction(['media'], 'readonly');
-            const store = transaction.objectStore('media');
-            const request = store.get(id);
+            const request = store.add({
+                ...mediaData,
+                timestamp: new Date().toISOString(),
+                likes: 0,
+                dislikes: 0
+            });
 
             request.onsuccess = () => resolve(request.result);
             request.onerror = () => reject(request.error);
@@ -96,39 +84,22 @@ class MyPubDB {
         });
     }
 
-    async toggleLike(mediaId, userId) {
+    async updateMediaLikes(id, likes, dislikes) {
         return new Promise((resolve, reject) => {
-            const transaction = this.db.transaction(['likes'], 'readwrite');
-            const store = transaction.objectStore('likes');
-            const request = store.get([mediaId, userId]);
+            const transaction = this.db.transaction(['media'], 'readwrite');
+            const store = transaction.objectStore('media');
+            const request = store.get(id);
 
             request.onsuccess = () => {
-                if (request.result) {
-                    store.delete([mediaId, userId]).onsuccess = () => resolve(false);
-                } else {
-                    store.add({
-                        mediaId,
-                        userId,
-                        timestamp: new Date()
-                    }).onsuccess = () => resolve(true);
-                }
+                const media = request.result;
+                media.likes = likes;
+                media.dislikes = dislikes;
+                
+                const updateRequest = store.put(media);
+                updateRequest.onsuccess = () => resolve();
+                updateRequest.onerror = () => reject(updateRequest.error);
             };
             request.onerror = () => reject(request.error);
         });
     }
-
-    async getLikesCount(mediaId) {
-        return new Promise((resolve, reject) => {
-            const transaction = this.db.transaction(['likes'], 'readonly');
-            const store = transaction.objectStore('likes');
-            const index = store.index('by_media');
-            const request = index.count(IDBKeyRange.only(mediaId));
-
-            request.onsuccess = () => resolve(request.result);
-            request.onerror = () => reject(request.error);
-        });
-    }
 }
-
-// Initialize database
-const db = new MyPubDB();
